660db2df752c5f850b4cb94f4f189459c71b1ee2
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-03-11 22:09:45 +0530

    addd

---
+++ 1_variable.js/Important_Definitions/imp.js
@@ -0,0 +1,7 @@
+
+// *******  What is NodeJS in JavaScript?
+
+// NodeJS is a cross-platform and opensource Javascript runtime environment
+//  that allows the javascript to be run on the server-side. 
+//  Nodejs allows Javascript code to run outside the browser. 
+//  Nodejs comes with a lot of modules and mostly used in web development.
