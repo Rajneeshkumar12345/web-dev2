f663d5e4c88997a600efceafe3d4248cc7491919
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-04-09 15:07:08 +0530

    add

--- 1_variable.js/5_Js_Concepts/HigherOrderFunction/Find.js
+++ 1_variable.js/5_Js_Concepts/HigherOrderFunction/Find.js
@@ -1,7 +1,7 @@
 
 
 // Find returns the first element of an array that satisfy the condition
-// Isme jaise hi first ele me condition apply hota hai vaise hi asn return kr deta
+// Whenever first time condition has satisfied this will return ans at that time 
 
 const transactions = [1000, 3000, 4000, 2000, -898, 3800, -4500];
 
