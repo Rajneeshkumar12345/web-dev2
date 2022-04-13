a5d1d0ea98c85b8a205cc1a59fceae1a748242d3
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-04-14 00:21:32 +0530

    add

--- 1_variable.js/12_React/Prerequisites/arrow.js
+++ 1_variable.js/12_React/Prerequisites/arrow.js
@@ -1,23 +1,55 @@
 // write a function to add two numbers and print their sum (function Expression)
 
 
-let add =(a , b)=>{ // It is called also Fat Arrow So no need to function
-    console.log(a+b)
+// let add =(a , b)=>{ // It is called also Fat Arrow So no need to function
+//     console.log(a+b)
+// }
+
+// add(3 , 4)
+
+
+// let regularFunction = function(name){
+//      console.log(`I am ${name}`)
+// }
+
+
+// let arrowFunction = (name) =>{
+//      console.log(`I am ${name}`)
+// }
+
+
+// regularFunction('Adam')
+
+// arrowFunction('Steve')
+
+
+// ******** Case -- 1*******//
+
+// let test = () => {
+//     console.log(this) // {} - Just because of empty element
+// }
+// test()
+
+
+
+
+// ******** Case -- 2  *******//
+
+
+let person = {
+       name : 'Adam',
+       age : 27 ,
+
+       showDetails : function(){
+              console.log(this.name + ' ' + this.age)
+      
+       },
+
+       showDetailsArrow : ()=>{
+              console.log(this.name + ' ' + this.age)
+       }
 }
 
-add(3 , 4)
 
-
-let regularFunction = function(name){
-     console.log(`I am ${name}`)
-}
-
-
-let arrowFunction = (name) =>{
-     console.log(`I am ${name}`)
-}
-
-
-regularFunction('Adam')
-
-arrowFunction('Steve')
+ person.showDetails()
+ person.showDetailsArrow() // Undefined - Becuase of emmpty(This) objects 
