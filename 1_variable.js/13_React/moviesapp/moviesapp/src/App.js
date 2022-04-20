d11216981a5cc4731054cfae8d9a69dc717db959
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-04-20 23:35:28 +0530

    addded

---
+++ 1_variable.js/13_React/moviesapp/moviesapp/src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+import NavBar from './components/NavBar';
+
+import Banner from './components/Banner';
+import MovieList from './components/MovieList';
+
+function App() {
+  return (
+    <>
+     <NavBar/>
+     <Banner />
+     <MovieList />
+    </>
+     
+
+  );
+}
+
+export default App;
