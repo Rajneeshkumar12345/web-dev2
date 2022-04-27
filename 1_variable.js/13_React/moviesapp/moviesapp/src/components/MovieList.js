141a438876755b89b7fb72d0ccf6428360a2692e
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-04-27 22:12:07 +0530

    class

--- 1_variable.js/13_React/moviesapp/moviesapp/src/components/MovieList.js
+++ 1_variable.js/13_React/moviesapp/moviesapp/src/components/MovieList.js
@@ -1,34 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-
-import { movies } from '../movieData'
+import { movies } from "../movieData";
 
 export class MovieList extends Component {
   render() {
-    let moviesArr = movies.results
-
+    let moviesArr = movies.results;
 
     return (
       <>
         <div>
-          <h3 className='text-center'> <strong>Trending</strong></h3>
+          <h3 className="text-center">
+            
+            <strong>Trending</strong>
+          </h3>
         </div>
 
-        <div className='movies-list'>
-          {
-            moviesArr.map((movieElem) => (
-              <div className="card movie-card" >
-                <img src="https://d2kektcjb0ajja.cloudfront.net/images/posts/feature_images/000/000/072/large-1466557422-feature.jpg?1466557422" style={{ height: '40vh', width: '20vw' }} className="card-img-top movie-img" alt="..." />
+        <div className="movies-list">
+          {moviesArr.map((movieElem) => (
+            <div className="card movie-card">
+              <img
+                src={`https://image.tmdb.org/t/p/original${movieElem.backdrop_path}`}
+                
+                style={{ height: "40vh", width: "20vw" }}
+                className="card-img-top movie-img"
+                alt="..."
+              />
 
-                <h5 className="card-title banner-title">{movieElem.title}</h5>
+              <h5 className="card-title movie-title">{movieElem.title}</h5>
 
-              </div>
-            ))
-          }
+               <div className="button-wrapper" style={{display : 'flex' , justifyContent:'center'}}>
+               <a href="#" className="btn btn-primary movies-button text-center">Add to Favourites</a>
+                  </div>
+    
+
+            
+            </div>
+          ))}
         </div>
       </>
-    )
+    );
   }
 }
 
-export default MovieList
+export default MovieList;
