289bd29e93736ed927d909cd8abceaeb76f65cee
Rajneeshkumar12345 <rkv7861999@gmail.com>
2022-05-12 22:34:08 +0530

    ad

---
+++ 1_variable.js/13_React/functionalreact/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+// import Test from './components/Test';
+import Demo from './components/Demo';
+
+function App() {
+  return (
+    <div >
+    {/* <Test/> */}
+    <Demo/>
+    </div>
+  );
+}
+
+export default App;
